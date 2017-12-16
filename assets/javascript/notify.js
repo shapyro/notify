a2367e2df238b12b7bc6f2de06643211a60a1ebf
shapyro <32754489+shapyro@users.noreply.github.com>
2017-12-16 14:36:57 -0600

    Update notify.js

--- assets/javascript/notify.js
+++ assets/javascript/notify.js
@@ -4,19 +4,19 @@ var apiKey = "E-mail hopefully coming";
 var buttonArray = [];
 	
 	//If we get freemusic archive API keys:
-	function displayBandInfo(){
-		var bandName = $(this).attr("data-name");
-		var queryURL = "https://freemusicarchive.org/api/trackSearch?q=" + bandName + "&limit=10";		
-		$.ajax({
-				url: queryURL,
-				method: "GET"
-			}).done(function(choice){
-				console.log(choice.data[0]);
-				for(i= 0; i<choice.data.length; i++){
-					$('#band').prepend("<img src=" + choice.data[i].images.original_still.url +" data state = 'still' style = 'height = 400px' class= 'gif'>");
-				}
-				})
-	}
+// 	function displayBandInfo(){
+// 		var bandName = $(this).attr("data-name");
+// 		var queryURL = "https://freemusicarchive.org/api/trackSearch?q=" + bandName + "&limit=10";		
+// 		$.ajax({
+// 				url: queryURL,
+// 				method: "GET"
+// 			}).done(function(choice){
+// 				console.log(choice.data[0]);
+// 				for(i= 0; i<choice.data.length; i++){
+// 					$('#band').prepend("<img src=" + choice.data[i].images.original_still.url +" data state = 'still' style = 'height = 400px' class= 'gif'>");
+// 				}
+// 				})
+// 	}
 
 
 
