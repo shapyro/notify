c762083bf9c0d083cad0f1830ced5f1c3d4c3e87
Greg Church <greggypc@users.noreply.github.com>
2017-12-20 18:07:36 -0600

    add media queries

--- assets/javascript/notify.js
+++ assets/javascript/notify.js
@@ -65,19 +65,19 @@ $(document).ready(function(){
           var lat = songkickData.resultsPage.results.event[i].venue.lat;
           var lng = songkickData.resultsPage.results.event[i].venue.lng;
 
-          $(".showlist").append(
-            '<div class="showDiv">' +
-            '<div class="row">' + 
-            '<div class="col-lg-3">' + 
-            '<img id="bandPic" src="https://images.sk-static.com/images/media/profile_images/artists/' + artistImage + '/huge_avatar" /></div>' +
-            '<div class="col-lg-9 bandInfo">' +
-            '<div class="row">' + 
-            '<div class="col-lg-6 artist">' + songkickData.resultsPage.results.event[i].performance["0"].artist.displayName + '</div>' +
-            '<div class="col-lg-6 venue">@ ' + songkickData.resultsPage.results.event[i].venue.displayName + '</div>' +
-            '</div>' +
-            '<div class="row">' + 
-            '<div class="col-lg-12 showDate">' + songkickData.resultsPage.results.event[i].start.date + '</div></div></div>'
-          );
+          $(".showlist").append(`
+            <div class="showDiv">
+            <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12">
+            <img id="bandPic" src="https://images.sk-static.com/images/media/profile_images/artists/ ${artistImage} /huge_avatar" /></div>
+            <div class="col-lg-9 col-md-9 col-sm-12 bandInfo">
+            <div class="row"> 
+            <div class="col-lg-6 col-md-12 artist"> ${songkickData.resultsPage.results.event[i].performance["0"].artist.displayName}</div>
+            <div class="col-lg-6 col-md-12 venue">@ ${songkickData.resultsPage.results.event[i].venue.displayName}</div>
+            </div>
+            <div class="row"> 
+            <div class="col-lg-12 col-md-12 showDate"> ${songkickData.resultsPage.results.event[i].start.date}</div></div></div>
+          `);
 
           $('.showDiv').data('lat', lat);
           $('.showDiv').data('lon', lng);
